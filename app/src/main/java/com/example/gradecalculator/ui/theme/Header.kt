2fe75185a579feda2693c973a4add13d8feb895a
johnwicj2 <tashonwashington598@gmail.com>
2026-04-09 01:41:56 -0500

    initial commit

---
+++ app/src/main/java/com/example/gradecalculator/ui/theme/Header.kt
@@ -0,0 +1,82 @@
+package com.example.gradecalculator.ui.theme
+
+import android.preference.PreferenceActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gradecalculator.R
+
+@Preview
+@Composable
+private fun HeaderPreview() {
+    Header()
+}
+
+@Composable
+fun Header() {
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Grade Calculator",
+            modifier = Modifier
+                .background(Color.Black)
+                .padding(16.dp)
+            ,
+            color = Color.Green,
+            fontSize = 30.sp
+
+        )
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .background(Color.Black)
+                .size(67.5.dp,67.5.dp),
+            alignment = Alignment.CenterEnd,
+            contentScale = ContentScale.FillWidth
+        )
+    }
+    Row(modifier = Modifier
+
+    ){
+        Text(
+            text = "Assignment Name",
+            modifier = Modifier
+                .padding(5.dp)
+            ,
+            color = Color.Black,
+            fontSize = 12.sp
+
+        )
+        Text(
+            text = "Grade",
+            modifier = Modifier
+                .padding(5.dp)
+                .padding(horizontal = 30.dp),
+            color = Color.Black,
+            fontSize = 12.sp
+
+        )
+        Text(
+            text = "Weight",
+            modifier = Modifier
+                .padding(5.dp)
+                .padding(horizontal = 30.dp),
+            color = Color.Black,
+            fontSize = 12.sp
+
+        )
+    }
+}
---
+++ app/src/main/java/com/example/gradecalculator/ui/theme/TextField.kt
@@ -0,0 +1,185 @@
+package com.example.gradecalculator.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+private fun TextFieldPreview(){
+    TextField()
+
+}
+data class GradeItem(
+    val name: String,
+    val grade: Double,
+    val weight: Double
+)
+@Composable
+fun TextField() {
+
+    var assignmentNames = remember {
+        mutableStateListOf("","","")
+    }
+    var grades = remember {
+        mutableStateListOf("","","")
+    }
+    var weights = remember {
+        mutableStateListOf("","","")
+    }
+    val gradeItems= remember { mutableStateListOf<GradeItem>() }
+    var finalGrade by remember { mutableStateOf(0.0) }
+    var count by remember { mutableStateOf(0) }
+
+    Row(horizontalArrangement = Arrangement.spacedBy(50.dp)) {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            contentPadding = PaddingValues(bottom = 20.dp, top = 10.dp)
+        ) {
+            items(3 + count) { i ->
+
+                OutlinedTextField(
+                    value = assignmentNames[i],
+                    onValueChange = { text ->
+                        assignmentNames[i] = text
+                        println(text)
+                    },
+                    modifier = Modifier
+                        .size(100.dp, 56.dp)
+
+                    ,
+
+
+                    )
+            }
+        }
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            contentPadding = PaddingValues(bottom = 20.dp, top = 10.dp)
+        ) {
+            items(3 + count) { i ->
+
+                OutlinedTextField(
+                    value = grades[i],
+                    onValueChange = { text ->
+                        grades[i] = text
+
+                    },
+                    modifier = Modifier
+                        .size(70.dp, 56.dp)
+
+                    ,
+
+
+                    )
+            }
+        }
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            contentPadding = PaddingValues(bottom = 20.dp, top = 10.dp)
+
+        ) {
+            items(3 + count) { i ->
+
+                OutlinedTextField(
+                    value = weights[i],
+                    onValueChange = { text ->
+                        weights[i] = text
+
+                    },
+                    modifier = Modifier
+                        .size(70.dp, 56.dp)
+
+                    ,
+
+
+                    )
+            }
+        }
+
+
+    }
+    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()
+        .padding(top = 20.dp)) {
+        Button(onClick ={
+            if (count < 5) {
+                count++
+                grades.add("")
+                weights.add("")
+                assignmentNames.add("")
+            }else {
+                println("too many rows")
+            }
+        }, colors = ButtonDefaults.buttonColors(Color.Black)) {
+            Text(text = "add more rows" , color = Color.Green)
+        }
+    }
+
+    Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.fillMaxWidth()
+        .padding(top = 20.dp)) {
+        Button(onClick ={
+            gradeItems.clear()
+            var totalWeighted = 0.0
+            var totalWeight = 0.0
+            for(i in grades.indices) {
+                val g = grades[i].toDouble()
+                val w = weights[i].toDouble()
+                val name  = assignmentNames[i]
+
+                gradeItems.add(GradeItem(name, g, w))
+                totalWeighted += g*w
+                totalWeight +=w
+            }
+            finalGrade = totalWeighted/ totalWeight
+
+
+        },colors = ButtonDefaults.buttonColors(Color.Black)) {
+            Text(text = "calculate grade",color = Color.Green)
+        }
+
+        LazyColumn(modifier = Modifier.padding(top = 20.dp)) {
+            items(gradeItems.size) {i ->
+                val item = gradeItems[i]
+                Text(
+                    text = "${item.name}: Grade ${item.grade}, Weight ${item.weight}"
+                )
+            }
+
+        }
+        Text(
+            text = "Final Grade: ${finalGrade}"
+
+        )
+
+
+
+
+
+
+
+
+
+    }
+
+}
